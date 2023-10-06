d29cd58959a68b58a96f6bdac792be25b6372b7d
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-06 15:24:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -prints the lowercase and  uppercase
+ *
+ * Retun: 0 Always (success)
+ */
+int main(void)
+{
+ putchar('a');
+ putchar('A');
+ putchar('\n');
+ return (0);
+}
