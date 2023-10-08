eaf5458fb9e23f16df0941ffd5c074a8adbbc9c1
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-08 03:28:53 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int num = 0;
+
+while (num <= 15)
+{
+if (num < 10)
+{
+putchar(num + '0');
+}
+else
+{
+putchar(num - 10 + 'a');
+}
+num++;
+}
+
+putchar('\n');
+
+return ('0');
+}
