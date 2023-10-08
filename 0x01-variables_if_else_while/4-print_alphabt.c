cedfc9fb93eba783ad430574e34c5025bac0ab4e
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-08 05:35:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,16 +5,19 @@
  *
  * Return: 0 Always (success)
  */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
- char c = 'a';
+char c = 'a';
 
- while (c <= 'z')
- {
-  if (c != 'e' && c != 'q')
-   putchar(c);
-  c++;
- }
- putchar('\n');
- return (0);
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
