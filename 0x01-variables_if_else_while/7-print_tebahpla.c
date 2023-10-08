e424f7cadbe6428a8d19955ec76e32355cc63c1a
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-08 03:40:17 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,13 +3,22 @@
  * main -prints the lowercase alphabet in reverse
  * followed by a new line.
  *
- * Return: 0 Always (success)
+ * Return: 0 Alway (success)
  */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	char low;
-         for(low='z'; low>='a'; low--)
-	   putchar(low);
-	   putchar('\n')
-         return (0);
+char letter = 'z';
+
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+
+putchar('\n');
+
+return ('0');
 }
