e1d810d6a32571ffadb0d197613856f5bfeb7e5a
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-10 11:16:15 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{print_alphabet_x10();
+return (0);
+}
