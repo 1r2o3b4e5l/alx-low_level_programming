d81109d9f95d9dd1e827774f6a73c5ab426e7ef5
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-10 13:19:49 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
+}
