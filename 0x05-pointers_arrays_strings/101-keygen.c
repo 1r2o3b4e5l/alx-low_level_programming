c2fce5d7b5c4cb22ee12aa1d3338dd680c8894c1
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-16 16:12:24 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
 
-int main(int argc, char **argv)
+/* betty style doc for function main goes there */
+int main(void)
 {
+int pass[100];
+int i, sum, n;
+sum = 0;	
 srand(time(NULL));
-if (argc != 2)
+for (i = 0; i < 100; i++)
 {
-printf("Usage: %s <program_name>\n", argv[0]);
-return 1;
-}
-char *password = malloc(65 * sizeof(char));
-if (password == NULL)
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
 {
-printf("Memory allocation error\n");
-return 1;
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
 }
-for (int i = 0; i < 64; i++)
-{
-password[i] = rand() % 94 + 33; // ASCII value range from 33 to 126
 }
-password[64] = '\0';
-printf("Password: %s\n", password);
-printf("Usage: %s \"%s\"\n", argv[1], password);
-free(password);
-return 0;
+return (0);
 }
 
