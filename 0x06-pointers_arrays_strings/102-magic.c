a96da5a81a837826d09c1cb848243f0cacf767a7
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-19 10:14:21 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
@@ -15,8 +17,8 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
-;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
