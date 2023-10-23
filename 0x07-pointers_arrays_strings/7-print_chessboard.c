9afb733adbb286240d9a8f5631b86d23b327856c
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-23 14:02:10 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_chessboard - prints chessboard
+ * @a: pointer
+ *
+ *Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+int aLen = sizeof(*a) / sizeof(*a[0]);
+for (i = 0; i < aLen; i++)
+{
+for (j = 0; j < aLen; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
