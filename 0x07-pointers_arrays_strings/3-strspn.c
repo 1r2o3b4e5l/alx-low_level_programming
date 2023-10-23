b27fbb2d8b28c893c7bd520146439a85e3f93867
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-10-23 13:05:04 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,32 +12,9 @@
  *
  * Return: Always n.
  */
-char *_strchr(char *s, char c)
-{
-while (*s)
-{
-if (*s == c)
-return (s);
-s++;
-}
-if (*s == c)
-return (s);
-return (0);
-}
-/**
- * _strspn - gets the length of a prefix substring.
- * @s: the string to be scanned
- * @accept: the string containing the list of characters to match in s.
- * Return: the number of bytes in the initial segment of s,
- * which consist only of bytes from accept.
- */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int ctr = 0;
-while (*(s + ctr))
-if (_strchr(accept, *(s + ctr)))
-ctr++;
-else
-break;
-return (ctr);
+unsigned int i;
+i = strspn(s, accept);
+return (i);
 }
